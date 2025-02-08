f135ed5b059923b27b555fbb739e23ca38d1728c
melina-thegarza <mjg2290@columbia.edu>
2025-02-08 17:12:39 -0500

    maintain chat history

--- kitchen/src/llm.js
+++ kitchen/src/llm.js
@@ -1,16 +1,28 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';  // Import the module
 
-// import dotenv from 'dotenv';
+const SYSTEM_PROMPT = `You are a health assistant named Daniel! Help the user create recipes that fit their fitness goals. You can assist with:
 
-// // Load the environment variables from the .env file
-// dotenv.config();
-export async function generateContentFromAI(prompt) {
+- Crafting meals tailored to user's fitness goals (e.g., weight loss, muscle gain).
+- Customizing recipes based on user's dietary preferences (e.g., vegetarian, keto).
+- Suggesting meals that match user's taste (e.g., sweet, spicy, savory).
+- Modifying recipes to include user's favorite ingredients or avoid ones they dislike.
+
+BE AS CONCISE AS POSSIBLE`;
+
+
+
+export async function generateContentFromAI(user_input, history) {
     try {
         // replace with GEMINI API key
         const apiKey = "";
         const genAI = new GoogleGenerativeAI(apiKey);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        const result = await model.generateContent(prompt);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" ,  systemInstruction: SYSTEM_PROMPT});
+        // const result = await model.generateContent(prompt);
+        const chat = model.startChat({
+            history: history,
+          });
+          
+        let result = await chat.sendMessage(user_input);
         return result.response.text();  // Return the generated content
     } catch (error) {
         console.error("Error generating content:", error);
